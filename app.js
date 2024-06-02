82bf63cc4d1769bd65f8788589d6be37878dedb9
jvelascocr <je.velasco85@gmail.com>
2024-06-02 23:30:48 +0200

    backend basico finalizado

--- app.js
+++ app.js
@@ -22,7 +22,30 @@ app.post(`/post`, async(req, res)=>{
     })
 })
 
-// Mostrar registros
+// Mostrar registros todos 
+app.get(`/posts`, async(req, res)=>{
+    const posts = await bd.post.findMany()
+    res.json(posts)
+})
+
+//Actualizar una bd
+app.put(`/post/:id`, async(req, res)=>{
+    const {id} = req.params
+    const {title, content} = req.body
+    const post = await bd.post.update({
+        where: {id:Number(id)},
+        data: {title, content}
+    })
+})
+
+//Eliminar registro bd
+app.delete(`/post/:id`, async(req,res)=>{
+    const {id} =req.params
+    const post = await bd.post.delete({
+        where: {id: Number(id)}
+    })
+    res.json('Eliminar')
+})
 
 app.listen(3000, ()=>
     console.log(`Servidor en http://localhost:3000`)
